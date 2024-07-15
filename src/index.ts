6c5905c04a96c4b717fd9123e96995e1057b5ec3
Cristian Petre <driimus@vivaldi.net>
2024-07-15 22:56:34 +0100

    feat: infer matcher type

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,23 @@
-type Entry<T> = T extends Array<infer E> ? E : T;
+type IterableEntry<T> = T extends Iterable<infer U> ? U : never;
 
-// biome-ignore lint/suspicious/noExplicitAny: <explanation>
-export type Match<T extends any[] = any[]> = [
-  output: unknown,
-  predicate: (...args: T) => boolean,
+// biome-ignore lint/suspicious/noExplicitAny: any parameter type goes
+export type Match<TArgs extends any[] = any[], TOut = unknown> = [
+  output: TOut,
+  predicate: (...args: TArgs) => boolean,
 ];
 
 export const matcher =
-  <T extends Match, TIter extends Iterable<T>>(checks: TIter) =>
-  (...args: Parameters<T[1]>): T[0] => {
+  <T extends Iterable<Match>>(checks: T) =>
+  (...args: Parameters<IterableEntry<T>[1]>): IterableEntry<T>[0] => {
     for (const [output, predicate] of checks) {
       if (predicate(...args)) return output;
     }
 
-    throw new Error('Exhaustive match did not return a value.');
+    throw new MatchNotFoundError();
   };
+
+export class MatchNotFoundError extends Error {
+  name = 'MatchNotFoundError' as const;
+
+  message = 'Exhaustive match did not return a value.';
+}
